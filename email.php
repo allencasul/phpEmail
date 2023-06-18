400921a3bbf44d769fc88eb51fbebbcffb573de4
Allen Charls Casul <120450666+allencasul@users.noreply.github.com>
2023-06-18 23:29:35 +0800

    Add files via upload

--- email.php
+++ email.php
@@ -16,13 +16,13 @@ if(isset($_POST['send'])){
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'charlscasul@gmail.com';
-    $mail->Password = 'oofqbtmwhlhllfkl';
+    $mail->Username = 'YOUR_EMAIL';
+    $mail->Password = 'YOUR_GOOGLE_APP_PASSWORD';
     $mail->Port = 465;
     $mail->SMTPSecure = 'ssl';
     $mail->isHTML(true);
     $mail->setFrom($email, $name);
-    $mail->addAddress('charlscasul@gmail.com');
+    $mail->addAddress('YOUR_EMAIL');
     $mail->Subject = ("$email ($subject)");
     $mail->Body = $message;
     $mail->send();
